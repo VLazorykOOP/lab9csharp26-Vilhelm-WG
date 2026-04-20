4a5c37244a779c76f7073865ad5c62d3aef5e832
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-04-20 14:37:32 +0300

    task_1

---
+++ Lab9_10CharpT/task_1/Compare_row.cs
@@ -0,0 +1,36 @@
+public class Compare_row
+{
+    public static bool IsReverse(string s1, string s2)
+    {
+        if (s1.Length != s2.Length) return false;
+
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in s1)
+        {
+            stack.Push(c);
+        }
+
+        foreach (char c in s2)
+        {
+            if (stack.Pop() != c)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void RunTask1()
+    {
+        Console.WriteLine("=== Завдання 1: Перевірка на зворотні рядки ===");
+        string string1 = "appl";
+        string string2 = "elppa";
+
+        if (IsReverse(string1, string2))
+            Console.WriteLine($"Рядок '{string2}' є оберненим до '{string1}'.");
+        else
+            Console.WriteLine("Рядки не є зворотними.");
+    }
+}
